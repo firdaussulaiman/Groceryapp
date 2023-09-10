b436b7c1ef640feb078d51cd6ed7375538684efd
jamesdai05 <dai_guilong@hotmail.com>
2023-09-11 07:04:18 +0800

    added the product schema

--- backend/express-groceries/server.js
+++ backend/express-groceries/server.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+//products rounter
 app.use("/", productsRouter);
 
 // catch 404 and forward to error handler
