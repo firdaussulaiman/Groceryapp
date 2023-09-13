ff227b7c39350d3afe8dedc88f95ff9db221790a
jamesdai05 <dai_guilong@hotmail.com>
2023-09-13 22:41:50 +0800

    add the timestamp to user schema

--- backend/express-groceries/Models/User.js
+++ backend/express-groceries/Models/User.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    Email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  Email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
