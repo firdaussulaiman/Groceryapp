40fc4a332459c5f6d7add218755b88d66fdf7b6f
jamesdai05 <dai_guilong@hotmail.com>
2023-09-13 22:43:47 +0800

    add the timestapm to lineItems schema

--- backend/express-groceries/Models/LineItem.js
+++ backend/express-groceries/Models/LineItem.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const lineItemSchema = new Schema({
-  cart: {
-    type: Schema.Types.ObjectId,
-    ref: "Cart",
+const lineItemSchema = new Schema(
+  {
+    cart: {
+      type: Schema.Types.ObjectId,
+      ref: "Cart",
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  product: {
-    type: Schema.Types.ObjectId,
-    ref: "Product",
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 const LineItems = mongoose.model("LineItem", lineItemSchema);
 module.exports = LineItems;
