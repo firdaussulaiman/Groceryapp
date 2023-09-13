26ddff41a4b03d4c43e3ac83280c22ec6ee321c4
jamesdai05 <dai_guilong@hotmail.com>
2023-09-13 22:40:07 +0800

    add the user schema

---
+++ backend/express-groceries/Models/LineItem.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const lineItemSchema = new Schema({
+  cart: {
+    type: Schema.Types.ObjectId,
+    ref: "Cart",
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const LineItems = mongoose.model("LineItem", lineItemSchema);
+module.exports = LineItems;
