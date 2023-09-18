514a36f4a55d80fb59b0f152922d79b81bc7e1ff
jamesdai05 <dai_guilong@hotmail.com>
2023-09-18 20:41:00 +0800

    add the user and the usersValidator file

--- backend/express-groceries/Models/User.js
+++ backend/express-groceries/Models/User.js
@@ -7,6 +7,7 @@ const userSchema = new Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
     Email: {
       type: String,
--- backend/express-groceries/controllers/cartController.js
+++ backend/express-groceries/controllers/cartController.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
 
-// const cartValidator = require("")
+const cartValidator = require("../joi-validators/Carts");
