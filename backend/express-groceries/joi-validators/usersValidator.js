89e2ec200e4568629397c11ee96a4d92aad4787b
jamesdai05 <dai_guilong@hotmail.com>
2023-09-22 13:49:22 +0800

    add the register and login validator

--- backend/express-groceries/joi-validators/usersValidator.js
+++ backend/express-groceries/joi-validators/usersValidator.js
@@ -4,7 +4,23 @@ const validators = {
   // registration validator:
   registerValidator: Joi.object({
     name: Joi.string().min(3).max(30).trim().label("Name").required(),
-    email: Joi.string().trim(),
-    git,
+    email: Joi.string()
+      .trim()
+      .email()
+      .min(5)
+      .minDomainSegments(2) //  to verify the given domain and minmum is 2 segments
+      .domain({ tlds: { allow: ["com", "net"] } }) // top-level-domain is com and net.
+      .label("Email")
+      .required(),
+    password: Joi.string().min(4).max(16).label("Password").required(),
+    confirmPassword: Joi.string()
+      .equal(Joi.ref("Password"))
+      .label("Confirm password")
+      .required(),
+  }),
+
+  loginValidator: Joi.object({
+    name: Joi.string().min(3).max(30).trim().label("Name").required(),
+    password: Joi.string().min(4).max(16).label("Password").required(),
   }),
 };
