8d17b171378e597115b3b1f814348b1a5ca17265
jamesdai05 <dai_guilong@hotmail.com>
2023-09-22 14:09:10 +0800

    add the edit and change password validator

--- backend/express-groceries/joi-validators/usersValidator.js
+++ backend/express-groceries/joi-validators/usersValidator.js
@@ -16,11 +16,34 @@ const validators = {
     confirmPassword: Joi.string()
       .equal(Joi.ref("Password"))
       .label("Confirm password")
-      .required(),
+      .required()
+      .messages({ "any.only": "{{lablel}} does not match!" }),
   }),
 
   loginValidator: Joi.object({
     name: Joi.string().min(3).max(30).trim().label("Name").required(),
     password: Joi.string().min(4).max(16).label("Password").required(),
   }),
+
+  editValidator: Joi.object({
+    name: Joi.string().min(3).max(30).trim().label("Name"),
+    email: Joi.string()
+      .trim()
+      .email()
+      .min(5)
+      .minDomainSegments(2)
+      .domain({ tlds: { allow: ["com", "net"] } })
+      .label("Email"),
+  }),
+  changePasswordValidator: Joi.object({
+    currentPassword: Joi.string().min(3).label("Current Password").required(),
+    newPassword: Joi.string().min(4).max(16).label("New Password").required(),
+    confirmNewPassword: Joi.string()
+      .equal(Joi.ref("newPassword"))
+      .label("Confirm new password")
+      .required()
+      .messages({ "any.only": "{{lablel}} does not match!" }),
+  }),
 };
+
+module.exports = validators;
