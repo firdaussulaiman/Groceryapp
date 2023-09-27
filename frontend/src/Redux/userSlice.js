7ec684139dd7a983ee6e866c97c9fcaf8c75c935
firdaussulaiman <firdausxwell@gmail.com>
2023-09-28 00:05:22 +0800

    ammendments5

--- frontend/src/Redux/userSlice.js
+++ frontend/src/Redux/userSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  name: "",
   email: "",
-  firstName: "",
-  image: "",
-  lastName: "",
-  _id: "",
+  
 };
 
 export const userSlice = createSlice({
@@ -15,18 +13,14 @@ export const userSlice = createSlice({
     loginRedux: (state, action) => {
       console.log(action.payload.data);
       //   state.user = action.payload.data;
-      state._id = action.payload.data._id;
-      state.firstName = action.payload.data.firstName;
-      state.lastName = action.payload.data.lastName;
+ 
+      state.name = action.payload.data.name;
       state.email = action.payload.data.email;
-      state.image = action.payload.data.image;
+
     },
     logoutRedux: (state, action) => {
-      state._id = "";
-      state.firstName = "";
-      state.lastName = "";
+      state.name = "";
       state.email = "";
-      state.image = "";
     },
   },
 });
