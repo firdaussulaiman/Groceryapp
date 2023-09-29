d60aa1c11665d716f0240b2372f0ea321333d367
jamesdai05 <dai_guilong@hotmail.com>
2023-09-29 21:30:46 +0800

    remove the unwant codes

--- frontend/src/page/ShoppingCart.js
+++ frontend/src/page/ShoppingCart.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const ShoppingCart = () => {
-  return (
-	<div>
-	  
-	</div>
-  )
-}
+  return <div></div>;
+};
 
-export default ShoppingCart
+export default ShoppingCart;
